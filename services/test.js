4fc921c4ac5df99e2f60828275da944ef4d3605e
var0g <alvaro.droppelmann.d@gmail.com>
2021-03-14 20:32:06 -0300

    Add: example how this work

--- services/test.js
+++ services/test.js
@@ -8,4 +8,48 @@ buttonAdd.onclick = function() {
     var row = tbodyEl.insertRow(); 
     var cell = row.insertCell(); 
     cell.innerHTML = add;
+    if (services.value == "service0") {
+        description = row.insertCell();
+        description.innerHTML = "Description for Service 0";
+        value = row.insertCell();
+        value.innerHTML = "3";
+        var image = document.createElement('img');
+        image.src = 'test.jpg';
+        image.width = "200";
+        ige = row.insertCell(); 
+        ige.appendChild(image);
+    }
+    if (services.value == "service1") {
+        description = row.insertCell();
+        description.innerHTML = "Description for Service 1";
+        value = row.insertCell();
+        value.innerHTML = "2 uf";
+        var image = document.createElement('img');
+        image.src = 'test.jpg';
+        image.width = "200";
+        ige = row.insertCell(); 
+        ige.appendChild(image);
+    }
+    if (services.value == "service2") {
+        description = row.insertCell();
+        description.innerHTML = "Description for Service 2";
+        value = row.insertCell();
+        value.innerHTML = "0.01";
+        var image = document.createElement('img');
+        image.src = 'test.jpg';
+        image.width = "200";
+        ige = row.insertCell(); 
+        ige.appendChild(image);
+    }
+    if (services.value == "service3") {
+        description = row.insertCell();
+        description.innerHTML = "Description for Service 3";
+        value = row.insertCell();
+        value.innerHTML = "10,00UF";
+        var image = document.createElement('img');
+        image.src = 'test.jpg';
+        image.width = "200";
+        ige = row.insertCell(); 
+        ige.appendChild(image);
+    }
 };
