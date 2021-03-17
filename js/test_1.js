e3505011d23088eb1a88d9383dc295644fac8be2
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-03-17 00:04:51 -0300

    organized a little

--- js/test_1.js
+++ js/test_1.js
@@ -9,62 +9,92 @@ buttonAdd.onclick = function() {
     var cell = row.insertCell(); 
     cell.innerHTML = add;
 
-    if (services.value == "General_Score") {
+    if (services.value == "General Score") {
         description = row.insertCell();
         description.innerHTML = "Texto para general Score";
+        var input = document.createElement('input');
+        input.setAttribute("type", "number");
+        input.setAttribute("value", "1");
+        input.min=1;
+        inu = row.insertCell();
+        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML =  "0.8" ;
+        value.innerHTML = input.valueAsNumber * 0.8;
         input.width = '50';
         var image = document.createElement('img');
-        image.src = "./photos/general.jpg";
+        image.src = '../photos/general.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "Score_Gill") {
+    if (services.value == "Score Gill") {
         description = row.insertCell();
         description.innerHTML = "Texto ejemplo para Score Gill";
+        var input = document.createElement('input');
+        input.setAttribute("type", "number");
+        input.setAttribute("value", "1");
+        input.min=1;
+        inu = row.insertCell();
+        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = "0.5";
+        value.innerHTML = input.valueAsNumber * 0.5;
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/gill.jpg';
+        image.src = '../photos/gill.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "Detección_de_FANs") {
+    if (services.value == "Detección de FANs") {
         description = row.insertCell();
         description.innerHTML = "Texto para la deteccion de fans";
+        var input = document.createElement('input');
+        input.setAttribute("type", "number");
+        input.setAttribute("value", "1");
+        input.min=1;
+        inu = row.insertCell();
+        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = "0.42";
+        value.innerHTML = input.valueAsNumber * 0.42;
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/fans.jpg';
+        image.src = '../photos/fans.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "H&E_organo") {
+    if (services.value == "H&E organo") {
         description = row.insertCell();
         description.innerHTML = "Texto ejemplo para h&e organo";
+        var input = document.createElement('input');
+        input.setAttribute("type", "number");
+        input.setAttribute("value", "1");
+        input.min=1;
+        inu = row.insertCell();
+        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = "0.6";
+        value.innerHTML = input.valueAsNumber * 0.6;
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/he_organo.jpg';
+        image.src = '../photos/he_organo.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "H&E_Alevin") {
+    if (services.value == "H&E Alevin") {
         description = row.insertCell();
         description.innerHTML = "Texto h&E alevin";
+        var input = document.createElement('input');
+        input.setAttribute("type", "number");
+        input.setAttribute("value", "1");
+        input.min=1;
+        inu = row.insertCell();
+        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = "1.1";
+        value.innerHTML = input.valueAsNumber * 1.1;
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/he_alevin.jpg';
+        image.src = '../photos/he_alevin.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
--- js/test_2.js
+++ js/test_2.js
@@ -9,92 +9,62 @@ buttonAdd.onclick = function() {
     var cell = row.insertCell(); 
     cell.innerHTML = add;
 
-    if (services.value == "General Score") {
+    if (services.value == "General_Score") {
         description = row.insertCell();
         description.innerHTML = "Texto para general Score";
-        var input = document.createElement('input');
-        input.setAttribute("type", "number");
-        input.setAttribute("value", "1");
-        input.min=1;
-        inu = row.insertCell();
-        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = input.valueAsNumber * 0.8 ;
+        value.innerHTML =  "0.8" ;
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/general.jpg';
+        image.src = "../photos/general.jpg";
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "Score Gill") {
+    if (services.value == "Score_Gill") {
         description = row.insertCell();
         description.innerHTML = "Texto ejemplo para Score Gill";
-        var input = document.createElement('input');
-        input.setAttribute("type", "number");
-        input.setAttribute("value", "1");
-        input.min=1;
-        inu = row.insertCell();
-        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = input.valueAsNumber * 0.5;
+        value.innerHTML = "0.5";
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/gill.jpg';
+        image.src = '../photos/gill.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "Detección de FANs") {
+    if (services.value == "Detección_de_FANs") {
         description = row.insertCell();
         description.innerHTML = "Texto para la deteccion de fans";
-        var input = document.createElement('input');
-        input.setAttribute("type", "number");
-        input.setAttribute("value", "1");
-        input.min=1;
-        inu = row.insertCell();
-        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = input.valueAsNumber * 0.42;
+        value.innerHTML = "0.42";
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/fans.jpg';
+        image.src = '../photos/fans.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "H&E organo") {
+    if (services.value == "H&E_organo") {
         description = row.insertCell();
         description.innerHTML = "Texto ejemplo para h&e organo";
-        var input = document.createElement('input');
-        input.setAttribute("type", "number");
-        input.setAttribute("value", "1");
-        input.min=1;
-        inu = row.insertCell();
-        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = input.valueAsNumber * 0.6;
+        value.innerHTML = "0.6";
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/he_organo.jpg';
+        image.src = '../photos/he_organo.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
     }
-    if (services.value == "H&E Alevin") {
+    if (services.value == "H&E_Alevin") {
         description = row.insertCell();
         description.innerHTML = "Texto h&E alevin";
-        var input = document.createElement('input');
-        input.setAttribute("type", "number");
-        input.setAttribute("value", "1");
-        input.min=1;
-        inu = row.insertCell();
-        inu.appendChild(input);
         value = row.insertCell();
-        value.innerHTML = input.valueAsNumber * 1.1;
+        value.innerHTML = "1.1";
         input.width = '50';
         var image = document.createElement('img');
-        image.src = './photos/he_alevin.jpg';
+        image.src = '../photos/he_alevin.jpg';
         image.width = "100";
         ige = row.insertCell(); 
         ige.appendChild(image);
