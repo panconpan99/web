649419a0f1397b4daa0e0f6b1e79226550a9215f
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-03-19 11:26:41 -0300

    start local sv and changed django version for compatibility

--- vehice/vehice/urls.py
+++ vehice/vehice/urls.py
@@ -1,7 +1,7 @@
 """vehice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_1 import views
 
 urlpatterns = [
+    path('',views.index,name="index"),
     path('admin/', admin.site.urls),
 ]
