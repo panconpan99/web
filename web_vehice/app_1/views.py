0f8645a154ebfcba292340e7224135c844aa076e
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-03-22 12:36:17 -0300

    added forms to django

--- web_vehice/app_1/views.py
+++ web_vehice/app_1/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from app_1 import forms
 # Create your views here.
 def index(request):
-    return render(request,'app_1/formulario_new.html')
+    form = forms.FormName()
+    return render(request,'app_1/formulario_new.html',{'form':form})
 
 def index_1(request):
     return render(request,'app_1/index_1.html')
