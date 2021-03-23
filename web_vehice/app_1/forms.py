cf0bb2212a2a91da9280348305361d3649ce0b83
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-03-23 01:58:35 -0300

    created tables and connected to a db

--- web_vehice/app_1/forms.py
+++ web_vehice/app_1/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class FormName(forms.Form):
     nombre = forms.CharField(max_length=30)
+    apellido = forms.CharField(max_length=30)
+    
--- web_vehice/app_1/models.py
+++ web_vehice/app_1/models.py
@@ -3,3 +3,35 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
+
+class represante(models.Model):
+    repr_name=models.CharField(max_length=30,unique=True)
+    repr_apellido=models.CharField(max_length=30,unique=True)
+    repr_correo=models.EmailField(max_length=50,unique=True)
+    telefono=models.CharField(max_length=20)
+    repr_centro=models.CharField(max_length=40)
+    repr_dir_centro=models.CharField(max_length=50)
+    id_repr=models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return "%s %s" % (self.repr_name, self.repr_apellido)
+
+class cotizacion(models.Model):
+    cantidad=models.IntegerField()
+    fecha=models.DateField(auto_now=True, auto_now_add=False)
+    id_cot=models.IntegerField(primary_key=True)
+
+class servicio(models.Model):
+    id_servicio=models.IntegerField(primary_key=True)
+    descripcion=models.CharField(max_length=255)
+    serv_name=models.CharField(max_length=30)
+    image_ruta=models.ImageField(upload_to='servicio')
+
+
+class repr_cot(models.Model):
+    repr_id=models.ForeignKey(represante,on_delete=models.CASCADE)
+    cot_id=models.ForeignKey(cotizacion,on_delete=models.CASCADE)
+
+class serv_cot(models.Model):
+    id_cot=models.ForeignKey(cotizacion,on_delete=models.CASCADE)
+    id_servicio=models.ForeignKey(servicio, on_delete=models.CASCADE)
