6c87b3335242bf1cdcdded7fcf3c9d85d5cfa891
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-03-24 12:18:17 -0300

    work in progress ,failed to connect 2 tables in one form

--- web_vehice/app_1/views.py
+++ web_vehice/app_1/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from app_1 import forms
+from .forms import Form_repr,Form_emp,Form_cot
 # Create your views here.
 def index(request):
-    form = forms.FormName()
-    return render(request,'app_1/formulario_new.html',{'form':form})
+    if request.method == 'POST':
+        form1=Form_repr(request.POST)
+        form2=Form_emp(request.POST)
+        if form1.is_valid() and form2.is_valid():
+            return HttpResponseRedirect('/index_1/')
+
+    return render(request,'app_1/formulario_new.html',{'form1':form1},{'form2':form2})
 
 def index_1(request):
     return render(request,'app_1/index_1.html')
