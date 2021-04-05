54fff12c6ae29041e1139b18877b30c8764b2a55
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-04-05 12:48:54 -0400

    added forms in services

--- web_vehice/app_1/views.py
+++ web_vehice/app_1/views.py
@@ -65,13 +65,17 @@ def result(request):
 @csrf_exempt
 def test(request):
     serv= Servicio.objects.all()
+    cot=[Cotizacion.objects.latest('id')]
+    print(cot)
     repre=Representante.objects.all()
     repreid=request.POST.get("id")
+    print(cot)
     if request.method == 'POST' and repreid != None:
         print(repreid)
         try:
             coti=Cotizacion(representante_id=repreid)
             coti.save()
+            cot=[Cotizacion.objects.latest('id')]
             #no salen los mensajes
             coti_data={"id":coti.id,"representante_id":coti.representante.id,"error":False,"ErrorMessage":"Cotización Creada"}
             return JsonResponse(coti_data,safe=False)
@@ -79,11 +83,24 @@ def test(request):
             print("error")
             coti_data={"error":True,"errorMessage":"Cotización fallida"}
             return JsonResponse(coti_data,safe=False)
-    #intentar mostrar la tabla de la cotización
-    return render(request,'app_1/test.html',{'repre':repre,'serv':serv})
+    return render(request,'app_1/test.html',{'repre':repre,'serv':serv,'cot':cot})
 
+@csrf_exempt
 def insertserv(request):
     idserv= request.POST.get("idserv")
+    servcount= request.POST.get("servcount")
+    servprecio = request.POST.get("servprecio")
+    cot=[Cotizacion.objects.latest('id')]
+    if request.method =='POST':
+        try:
+            servicio=Servicio.objects.get(id=idserv)
+            serv=Cotizacion.servicio.add(servicio,through_default={'cantidad':servcount,'nuevo_precio':servprecio})
+            serv.save()
+            serv_data={"id":serv.id,"nombre":serv.servicio.nombre}
+            return JsonResponse(serv_data,safe=False)
+        except:
+            print("fallo")
+            pass 
 
 
     
