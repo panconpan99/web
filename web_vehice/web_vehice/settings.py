83b5241a92e01c8a3ebee85add27b7b5bc28ca32
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2023-10-29 22:08:35 -0300

    minor changes

--- web_vehice/web_vehice/settings.py
+++ web_vehice/web_vehice/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR= os.path.join(BASE_DIR,'static')
 # SECURITY WARNING: keep the secret key used in production secret!
 env = environ.Env()
 environ.Env.read_env()
-SECRET_KEY = env("secret_key")
+SECRET_KEY = env('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
